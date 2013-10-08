bbb8f70f56a2e366f1fbbc71bc18db4046f4dcc4
Pedro Borges <highvolt16@gmail.com>
2013-10-08 18:23:07 +0100

    auth with {"username":"teste","password":"1234"}

--- server/server.js
+++ server/server.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
 
+
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
+
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.send('hello world2');
+});
+
+app.post('/auth_user',function(req,res){
+	res.set('Content-Type','application/json');
+	if(req.get('Content-Type')!="application/json"){
+		res.status(400).send(JSON.stringify({'error':'needs to be JSON'}));
+		return;
+	}
+	if(req.body.username=="teste" && req.body.password=="1234"){
+		res.status(200).send(JSON.stringify({'sessionId':'abcdefgh'}));
+	}
+	
+	res.status(401).send(JSON.stringify('invalid login'));
 });
 
 app.listen(3000);
