ecd6263e639c76024825ec979764623c0ec63666
Pedro Borges <highvolt16@gmail.com>
2013-10-13 18:03:20 +0100

    auth and buy still no db

--- server/server.js
+++ server/server.js
@@ -24,4 +24,16 @@ app.post('/auth_user',function(req,res){
 	res.status(401).send(JSON.stringify('invalid login'));
 });
 
+
+app.post('/buy_ticket',function(req,res){
+	res.set('Content-Type','application/json');
+	if(req.get('Content-Type')!="application/json"){
+		res.status(400).send(JSON.stringify({'error':'needs to be JSON'}));
+		return;
+	}
+	
+	
+	res.status(401).send(JSON.stringify('invalid login'));
+});
+
 app.listen(3000);
