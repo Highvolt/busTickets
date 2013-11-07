2584b09a475761e7a3f8cb5e2f893a0025d89f24
Marco Amador <marco.amador91@gmail.com>
2013-11-07 03:11:12 +0000

    Erro nos dialogs corrigidos. Ecrã de registo actualizado e melhorado.

--- workspace/PassengerApp/src/com/example/passengerapp/RegisterActivity.java
+++ workspace/PassengerApp/src/com/example/passengerapp/RegisterActivity.java
@@ -1,15 +1,82 @@
 package com.example.passengerapp;
 
+import java.lang.reflect.Field;
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class RegisterActivity extends Activity {
+	
+	private int pickerYear = 0;
+    private int pickerMonth = 0;
+    private int pickerDay = 0;
+    
+    DatePickerDialog.OnDateSetListener pickerDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                pickerYear = year;
+                pickerMonth = monthOfYear;
+                pickerDay = dayOfMonth;
+                
+                int localMonth = (pickerMonth + 1);
+                String monthString = localMonth < 10 ? "0" + localMonth : Integer.toString(localMonth);
+                String localYear = Integer.toString(pickerYear).substring(2);
+                ((EditText) findViewById(R.id.reg_ccdate)).setText(new StringBuilder().append(monthString).append("/").append(localYear).append(" "));
+        }
+
+    };
+
+	private DatePickerDialog createCustomDatePicker() {
+	    DatePickerDialog dpd = new DatePickerDialog(this, pickerDateSetListener, pickerYear, pickerMonth, pickerDay);
+	    try {
+	            Field[] datePickerDialogFields = dpd.getClass().getDeclaredFields();
+	            for (Field datePickerDialogField : datePickerDialogFields) {
+	                    if (datePickerDialogField.getName().equals("mDatePicker")) {
+	                            datePickerDialogField.setAccessible(true);
+	                            DatePicker datePicker = (DatePicker) datePickerDialogField.get(dpd);
+	                            Field datePickerFields[] = datePickerDialogField.getType().getDeclaredFields();
+	                            for (Field datePickerField : datePickerFields) {
+	                                    if ("mDayPicker".equals(datePickerField.getName()) || "mDaySpinner".equals(datePickerField.getName())) {
+	                                            datePickerField.setAccessible(true);
+	                                            Object dayPicker = new Object();
+	                                            dayPicker = datePickerField.get(datePicker);
+	                                            ((View) dayPicker).setVisibility(View.GONE);
+	                                    }
+	                            }
+	                    }
+	
+	            }
+	    } catch (Exception ex) {}
+	    return dpd;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+		Calendar c = Calendar.getInstance();
+        pickerYear = c.get(Calendar.YEAR);
+        pickerMonth = c.get(Calendar.MONTH) + 1;
+		
+		EditText datePicker = (EditText) findViewById(R.id.reg_ccdate);
+		datePicker.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				createCustomDatePicker().show();
+			}
+			
+		});
+		
 	}
 
 	@Override
--- workspace/PassengerApp/src/com/example/passengerapp/SettingsActivity.java
+++ workspace/PassengerApp/src/com/example/passengerapp/SettingsActivity.java
@@ -28,7 +28,7 @@ public class SettingsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		ListView list;
-		String list_array[]={"Credit Card","E-mail","Password"};
+		String list_array[]={"Credit Card","Password"};
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 		list=(ListView)findViewById(R.id.listView1);
@@ -38,50 +38,19 @@ public class SettingsActivity extends Activity {
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
 					long id) {
-				if(position == 2){
-					LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				       final View layout = inflater.inflate(R.layout.change_password, (ViewGroup) findViewById(R.id.root));
-				       final EditText password1 = (EditText) layout.findViewById(R.id.EditText_Pwd1);
-				       final EditText password2 = (EditText) layout.findViewById(R.id.EditText_Pwd2);
-				       final TextView error = (TextView) layout.findViewById(R.id.TextView_PwdProblem);
-				       password2.addTextChangedListener(new TextWatcher() {
-				    	   public void afterTextChanged(Editable s) {
-				    	      String strPass1 = password1.getText().toString();
-				    	      String strPass2 = password2.getText().toString();
-				    	      if (strPass1.equals(strPass2)) {
-				    	         error.setText("Passwords match");
-				    	      } else {
-				    	         error.setText("Passwords don't match");
-				    	      }
-				    	   }
-				    	   public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-				    	   public void onTextChanged(CharSequence s, int start, int before, int count) {}
-				    	 });
-				       AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-				       builder.setTitle("Enter Password");
-				       builder.setView(layout);
-				       builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-				    	   public void onClick(DialogInterface dialog, int whichButton) {
-				    	      removeDialog(MY_PASSWORD_DIALOG_ID);
-				    	   }
-				    	});
-				    	builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-				    	   public void onClick(DialogInterface dialog, int which) {
-				    	      String strPassword1 = password1.getText().toString();
-				    	      String strPassword2 = password2.getText().toString();
-				    	      if (strPassword1.equals(strPassword2)) {
-				    	         Toast.makeText(SettingsActivity.this,
-				    	            "Matching passwords="+strPassword2, Toast.LENGTH_SHORT).show();
-				    	      }
-				    	      removeDialog(MY_PASSWORD_DIALOG_ID);
-				    	   }
-				    	});
-				    	builder.show();
-				       
+				if(position == 0){
+					createCreditCardDialog();			       
 				}
-				
+				if(position == 1){
+					createPasswordDialog();			       
+				}				
 			}
 		});
+	}
+
+	public void createCreditCardDialog() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
@@ -89,6 +58,42 @@ public class SettingsActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void createPasswordDialog(){
+		   LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	       final View layout = inflater.inflate(R.layout.change_password, (ViewGroup) findViewById(R.id.root));
+	       final EditText password1 = (EditText) layout.findViewById(R.id.EditText_Pwd1);
+	       final EditText password2 = (EditText) layout.findViewById(R.id.EditText_Pwd2);
+	       final TextView error = (TextView) layout.findViewById(R.id.TextView_PwdProblem);
+	       
+	       AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	       builder.setTitle("Change Password");
+	       builder.setView(layout);
+	       builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+	    	   public void onClick(DialogInterface dialog, int whichButton) {
+	    	      removeDialog(MY_PASSWORD_DIALOG_ID);
+	    	   }
+	    	});
+	    	builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+	    	   public void onClick(DialogInterface dialog, int which) {
+	    	      String strPassword1 = password1.getText().toString();
+	    	      String strPassword2 = password2.getText().toString();
+	    	      if (strPassword1.equals(strPassword2)) {
+	    	         Toast.makeText(SettingsActivity.this,
+	    	            "Updating Password", Toast.LENGTH_LONG).show();
+	    	         //TODO Fazer cenas
+	    	         
+	    	         removeDialog(MY_PASSWORD_DIALOG_ID);
+	    	      }
+	    	      else{
+	    	    	  error.setText("Passwords don't match");
+	    	    	  error.setTextColor(0xff0000);
+	    	      }
+	    	      
+	    	   }
+	    	});
+	    	builder.show();
 	}
 	
 	public boolean onOptionsItemSelected(MenuItem item) {
